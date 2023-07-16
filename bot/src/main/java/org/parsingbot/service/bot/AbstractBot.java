090fcadecffa7e1b120ca160298726c8cfbb59cc
Kirill <kirill.ustimencko@gmail.com>
2023-07-16 19:46:54 +0300

    рефакторинг

--- bot/src/main/java/org/parsingbot/service/bot/AbstractBot.java
+++
@@ -1,8 +0,0 @@
-package org.parsingbot.service.bot;
-
-import org.parsingbot.configuration.BotParametersProvider;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-
-public abstract class AbstractBot extends TelegramLongPollingBot {
-
-}
