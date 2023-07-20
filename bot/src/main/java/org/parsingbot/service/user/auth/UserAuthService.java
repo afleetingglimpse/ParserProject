350390c9ee131625474e674909b31de20a0a6f4c
Kirill <kirill.ustimencko@gmail.com>
2023-07-20 23:59:38 +0300

    минорные изменения

--- bot/src/main/java/org/parsingbot/service/user/auth/UserAuthService.java
+++ bot/src/main/java/org/parsingbot/service/user/auth/UserAuthService.java
@@ -8,10 +8,11 @@ public interface UserAuthService {
     /**
      * Проверка авторизации пользователей
      *
-     * @param userName имя пользователя
+     * @param userName      имя пользователя
+     * @param authorisation необходимая авторизация
      * @return true/false если у пользователя есть/нет прав для просмотра
      */
-    boolean isAuthorised(String userName);
+    boolean isAuthorised(String userName, Authorisation authorisation);
 
     /**
      * Установка авторизации пользователя по userName
