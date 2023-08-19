a623d835e600da2bbb41bb63c564296de12c23f5
Kirill <kirill.ustimencko@gmail.com>
2023-08-19 13:38:17 +0300

    фикс

--- commons/src/main/java/org/parsingbot/repository/UserRepository.java
+++ commons/src/main/java/org/parsingbot/repository/UserRepository.java
@@ -2,6 +2,15 @@ package org.parsingbot.repository;
 
 import org.parsingbot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUserName(String userName);
+
+    @Query(value = "select * from users where is_subscribed = 'true'", nativeQuery = true)
+    List<User> getSubscribedUsers();
 }
