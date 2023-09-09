af339b4da184b11b94c75b96a1c7f48fc9e29c5e
Kirill <kirill.ustimencko@gmail.com>
2023-09-09 21:28:22 +0300

    minor fix

--- bot/src/main/java/org/parsingbot/service/receiver/checkers/CommandAuthorisationChecker.java
+++ bot/src/main/java/org/parsingbot/service/receiver/checkers/CommandAuthorisationChecker.java
@@ -12,7 +12,7 @@ public interface CommandAuthorisationChecker {
      * Метод проверки авторизации пользователя для команды
      *
      * @param commandDto обертка над сообщением-командой пользователя
-     * @param user объект пользователя
+     * @param user       объект пользователя
      * @return сообщение об ошибке или null
      */
     String checkCommandAuthorisation(CommandDto commandDto, User user);
