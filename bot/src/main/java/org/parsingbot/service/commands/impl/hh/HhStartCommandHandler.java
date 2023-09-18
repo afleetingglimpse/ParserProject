6d450645ae191428b1d4f7050c5c736f2d056046
Kirill <kirill.ustimencko@gmail.com>
2023-09-18 21:03:56 +0300

    Поменял чутка UserService, добавил HhStartCommandHandler

--- bot/src/main/java/org/parsingbot/service/commands/impl/hh/HhStartCommandHandler.java
+++ bot/src/main/java/org/parsingbot/service/commands/impl/hh/HhStartCommandHandler.java
@@ -1,10 +1,12 @@
 package org.parsingbot.service.commands.impl.hh;
 
 import lombok.RequiredArgsConstructor;
-import org.parsingbot.entity.Command;
+import org.parsingbot.entity.Event;
+import org.parsingbot.entity.State;
+import org.parsingbot.service.UserService;
 import org.parsingbot.service.commands.CommandHandler;
+import org.parsingbot.util.BotUtils;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
-import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.Serializable;
 import java.util.List;
@@ -12,8 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HhStartCommandHandler implements CommandHandler {
 
+    private static final String GREETING_TEXT = "Введите название вакансии для поиска. Или оставьте пустым, чтобы увидеть" +
+            "вакансии для Java-разработчиков";
+
+    private final UserService userService;
+
     @Override
-    public List<PartialBotApiMethod<? extends Serializable>> handleCommand(Command command, Update update) {
-        return null;
+    public List<PartialBotApiMethod<? extends Serializable>> handleCommand(Event event) {
+        userService.updateStateByUser(event.getUser(), State.HH_1);
+        return List.of(BotUtils.createMessage(event.getChatId(), GREETING_TEXT));
     }
 }
