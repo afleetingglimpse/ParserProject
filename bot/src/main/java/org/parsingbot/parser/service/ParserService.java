f00457d100766be64c54ac9dcb15782dd3a8c11b
Kirill <kirill.ustimencko@gmail.com>
2023-09-21 13:51:38 +0300

    scheduler fix

--- bot/src/main/java/org/parsingbot/parser/service/ParserService.java
+++ bot/src/main/java/org/parsingbot/parser/service/ParserService.java
@@ -1,9 +1,12 @@
 package org.parsingbot.parser.service;
 
 import org.parsingbot.entity.Event;
+import org.parsingbot.entity.User;
+import org.parsingbot.entity.Vacancy;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Сервис для взаимодействия с парсером в удобном для бота формате
@@ -12,9 +15,29 @@ public interface ParserService {
 
     /**
      * @param event обертка над update, user и command
+     * @return список вакансий
+     */
+    List<Vacancy> getVacancies(Event event);
+
+    /**
+     * @param user              объект пользователя
+     * @param parsingParameters параметры поиска
+     * @return список вакансий
+     */
+    List<Vacancy> getVacancies(User user, Map<String, String> parsingParameters);
+
+    /**
+     * @param event обертка над update, user и command
      * @return список сообщений для пользователя (каждое сообщение содержит по одной вакансии)
      */
     List<SendMessage> getVacanciesMessageList(Event event);
+
+    /**
+     * @param user              объект пользователя
+     * @param parsingParameters параметры поиска
+     * @return список сообщений для пользователя (каждое сообщение содержит по одной вакансии)
+     */
+    List<SendMessage> getVacanciesMessageList(User user, Map<String, String> parsingParameters);
 
     /**
      * @param event обертка над update, user и command
