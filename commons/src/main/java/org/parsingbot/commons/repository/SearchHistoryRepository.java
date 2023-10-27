dbdeb51e975eea6599cdca5e3ff48197d2565655
Kirill <kirill.ustimencko@gmail.com>
2023-10-27 22:23:01 +0300

    Добавил SearchHistoryRepository

---
+++ commons/src/main/java/org/parsingbot/commons/repository/SearchHistoryRepository.java
@@ -0,0 +1,12 @@
+package org.parsingbot.commons.repository;
+
+import org.parsingbot.commons.entity.SearchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Integer> {
+
+    @Query(value = "select * from search_history where user_id = ?",
+            nativeQuery = true)
+    SearchHistory getSearchHistoryByUserId(Long userId);
+}
