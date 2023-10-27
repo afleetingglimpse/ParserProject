71388fc75764176aa71ebb7e16fb2a95cd44273e
Kirill <kirill.ustimencko@gmail.com>
2023-10-27 21:55:17 +0300

    Добавил SearchHistory

---
+++ commons/src/main/java/org/parsingbot/commons/entity/SearchHistory.java
@@ -0,0 +1,26 @@
+package org.parsingbot.commons.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter
+@Getter
+@Table(name = "search_history")
+public class SearchHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    //    private Integer userId;
+    private String vacancyName;
+    private Integer numberOfVacancies;
+    private String keywords;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
